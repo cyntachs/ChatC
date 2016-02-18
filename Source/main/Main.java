e145b9e1f3a6ca11fc86449d2ef6e3edd688f2a9
Cyntachs <edrendacaymat@gmail.com>
2016-02-17 19:34:17 -0500

    started on the interface

--- Source/main/Main.java
+++ Source/main/Main.java
@@ -1,10 +1,27 @@
+// package
 package main;
 
-public class Main {
+import java.awt.Container;
 
+// imports
+import javax.swing.*;
+
+// main program
+public class Main extends JFrame {
+	public static final String AppName = "ChatC";
+	
+	// frame initialize
+	public Main() {
+		super(AppName);
+		//setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(null);
+		setSize(500,450);
+		setVisible(true);
+	};
+	
+	// main function
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// create frame
+		Main Frame = new Main();
 	}
-
 }
