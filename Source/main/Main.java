6e10dbb34d76651be826d154fb4d5238f4c9087d
cyntachs <edrendacaymat@gmail.com>
2016-02-18 23:38:35 -0500

    Changed window layout. Added placeholder textboxes.
    Experimenting with window layouts. This one currently looks simple and
    nice.

--- Source/main/Main.java
+++ Source/main/Main.java
@@ -2,8 +2,8 @@
 package main;
 
 //imports
-import java.awt.Button;
-import java.awt.Container;
+import java.awt.*;
+
 import javax.swing.*;
 
 // main program
@@ -15,9 +15,28 @@ public class Main extends JFrame {
 		super(AppName);
 		//setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		Container mContainer = getContentPane();
-		mContainer.setLayout(new BoxLayout(mContainer, BoxLayout.Y_AXIS));
-		add(new Button("1"));
-		add(new Button("2"));
+		mContainer.setLayout(new BorderLayout());
+		
+		Container tContainer = new Container();
+		tContainer.setLayout(new BorderLayout());
+		
+		mContainer.add(tContainer, BorderLayout.CENTER);
+		
+		JTextArea txtAreaLeft = new JTextArea("LEFT (Chat history area)"); 
+		JScrollPane scrollPane = new JScrollPane(txtAreaLeft); 
+		txtAreaLeft.setEditable(false);
+		
+		JTextArea txtAreaRight = new JTextArea("RIGHT (Users in session)"); 
+		JScrollPane scrollPane2 = new JScrollPane(txtAreaRight); 
+		txtAreaRight.setEditable(false);
+		
+		JTextArea txtAreaInput = new JTextArea("INPUT (Text input)"); 
+		JScrollPane scrollPane3 = new JScrollPane(txtAreaInput); 
+		txtAreaInput.setEditable(false);
+		
+		tContainer.add(scrollPane, BorderLayout.CENTER);
+		tContainer.add(scrollPane2, BorderLayout.LINE_END);
+		mContainer.add(scrollPane3, BorderLayout.PAGE_END);
 		setSize(500,450);
 		setVisible(true);
 	};
@@ -25,6 +44,6 @@ public class Main extends JFrame {
 	// main function
 	public static void main(String[] args) {
 		// create frame
-		Main Frame = new Main();
+		new Main();
 	}
 }
